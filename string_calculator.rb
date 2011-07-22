40890dbd46e4dac1126fc5abff8314db65a395a2
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-22 12:54:28 +0200

    String calculator #3

--- string_calculator.rb
+++ string_calculator.rb
@@ -9,4 +9,8 @@ describe "The string calculator method `Add'" do
   it "returns 0 for an empty string" do
     add("").should == 0
   end
+
+  it "returns the given digit if the string consists of just one digit" do
+    add("1").should == 1
+  end
 end
