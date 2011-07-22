db11e85a4725b6107a03a9add0ba8dcb714d3df9
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-22 12:53:15 +0200

    String calculator #2

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'mac_bacon'
 
+def add(string)
+  0
+end
+
 describe "The string calculator method `Add'" do
   it "returns 0 for an empty string" do
     add("").should == 0
