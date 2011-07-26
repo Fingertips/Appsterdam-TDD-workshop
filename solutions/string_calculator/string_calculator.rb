cb13ecb0a2ffc07cbec5af15bded5fb15432bc45
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-26 23:01:38 +0200

    Add some more docs.

--- solutions/string_calculator/string_calculator.rb
+++ solutions/string_calculator/string_calculator.rb
@@ -1,7 +1,18 @@
 # Original kata from: http://osherove.com/tdd-kata-1
 #
-# 1. First make sure the mac_bacon-1.3.gem is installed
+# 1. First make sure the MacBacon Ruby gem is installed:
+#      $ macgem install mac_bacon-1.3.gem
 # 2. Run the test with: $ macbacon string_calculator.rb
+#
+# If you want to automatically run the test everytime a file is saved, you can
+# do so with the Kicker Ruby gem:
+#
+# 1. Install the dependency:
+#      $ macgem install rb-fsevent-0.4.1.gem
+# 2. Then install the actual Kicker gem:
+#      $ macgem install kicker-2.3.1.gem
+# 3. Finally, run the test like so:
+#      $ kicker -c -e 'macbacon string_calculator.rb'
 
 require 'rubygems'
 require 'mac_bacon'
