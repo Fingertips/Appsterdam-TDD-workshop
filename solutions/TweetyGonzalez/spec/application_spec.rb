060ac4dc350a3243f7a93749437859ddd1d53913
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-28 00:31:39 +0200

    Write specs that need to be implemented.

---
+++ solutions/TweetyGonzalez/spec/application_spec.rb
@@ -0,0 +1,22 @@
+describe "Tweet" do
+  it "performs a search on the remote webservice" do
+  end
+
+  it "returns the author's name" do
+  end
+
+  it "returns the message" do
+  end
+end
+
+describe "WindowController" do
+  it "enables the search button if the textfield contains text" do
+  end
+
+  it "performs a search on the remote webservice" do
+    # stub expect
+  end
+
+  it "shows the search results in the tableview" do
+  end
+end
