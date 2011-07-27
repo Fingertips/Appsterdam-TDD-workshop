10199003be18b34acf53f0bad91868fefa6fbf9d
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-28 00:23:10 +0200

    Create view programatically.

--- solutions/TweetyGonzalez/TweetyGonzalez/TweetyGonzalezAppDelegate.h
+++ solutions/TweetyGonzalez/TweetyGonzalez/TweetyGonzalezAppDelegate.h
@@ -1,17 +1,15 @@
-//
-//  TweetyGonzalezAppDelegate.h
-//  TweetyGonzalez
-//
-//  Created by Eloy Duran on 27-07-11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
+@class WindowController;
+
 @interface TweetyGonzalezAppDelegate : NSObject <NSApplicationDelegate> {
-    NSWindow *window;
+  WindowController *windowController;
 }
 
-@property (assign) IBOutlet NSWindow *window;
+@property (retain) WindowController *windowController;
 
+@end
+
+
+@interface WindowController : NSWindowController
 @end
--- solutions/TweetyGonzalez/TweetyGonzalez/TweetyGonzalezAppDelegate.m
+++ solutions/TweetyGonzalez/TweetyGonzalez/TweetyGonzalezAppDelegate.m
@@ -1,20 +1,47 @@
-//
-//  TweetyGonzalezAppDelegate.m
-//  TweetyGonzalez
-//
-//  Created by Eloy Duran on 27-07-11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import "TweetyGonzalezAppDelegate.h"
 
 @implementation TweetyGonzalezAppDelegate
 
-@synthesize window;
+@synthesize windowController;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+  self.windowController = [[WindowController new] autorelease];
+  [windowController showWindow:self];
+}
+
+@end
+
+
+@implementation WindowController
+
+- (id)init {
+  NSWindow *win = [[[NSWindow alloc] initWithContentRect:NSMakeRect(50, 50, 500, 400)
+                                               styleMask:NSTitledWindowMask|NSResizableWindowMask
+                                                 backing:NSBackingStoreBuffered
+                                                   defer:NO] autorelease];
+  if ((self = [super initWithWindow:win])) {
+    NSView *contentView = [win contentView];
+
+    NSScrollView *scrollView = [[[NSScrollView alloc] initWithFrame:NSMakeRect(0, 48, 500, 352)] autorelease];
+    scrollView.autoresizingMask = NSViewWidthSizable|NSViewHeightSizable;
+    [contentView addSubview:scrollView];
+
+    NSTableView *tableView = [[[NSTableView alloc] initWithFrame:NSMakeRect(0, 0, 500, 352)] autorelease];
+    scrollView.documentView = tableView;
+
+    NSTextField *textField = [[[NSTextField alloc] initWithFrame:NSMakeRect(10, 10, 390, 28)] autorelease];
+    textField.autoresizingMask = NSViewWidthSizable;
+    [contentView addSubview:textField];
+
+    NSButton *button = [[[NSButton alloc] initWithFrame:NSMakeRect(410, 10, 80, 28)] autorelease];
+    button.title = @"Search";
+    button.enabled = NO;
+    button.bezelStyle = NSRoundedBezelStyle;
+    button.autoresizingMask = NSViewMinXMargin;
+    [contentView addSubview:button];
+  }
+  return self;
 }
 
 @end
