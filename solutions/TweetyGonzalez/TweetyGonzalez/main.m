ea401572aba3f69317e867c9bb708862379f1c16
Eloy Duran <eloy.de.enige@gmail.com>
2011-07-27 23:38:55 +0200

    Add app skeleton.

---
+++ solutions/TweetyGonzalez/TweetyGonzalez/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TweetyGonzalez
+//
+//  Created by Eloy Duran on 27-07-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
